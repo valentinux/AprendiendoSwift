e64b91be0511b9d210d7e19f68a4ff537420bc65
valentin <email@email.com>
2024-07-24 15:11:23 +0200

    first commit

---
+++ aprendiendoswift/aprendiendoswift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aprendiendoswift
+//
+//  Created by Valentin Fernandez on 24/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aprendiendoswift/aprendiendoswift/aprendiendoswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  aprendiendoswiftApp.swift
+//  aprendiendoswift
+//
+//  Created by Valentin Fernandez on 24/7/24.
+//
+
+import SwiftUI
+
+@main
+struct aprendiendoswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
