fbc07dcecded800e5fbcf375f28a15256298fbd8
valentin <email@email.com>
2024-07-31 21:38:21 +0200

    commit images

--- Formacion02-imagenes/Formacion02-imagenes/ContentView.swift
+++ Formacion02-imagenes/Formacion02-imagenes/ContentView.swift
@@ -11,11 +11,30 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "externaldrive.fill.badge.exclamationmark")
+                .symbolRenderingMode(.palette)
                 .font(.system(size: 80))
-                .foregroundStyle(.purple)
+                .foregroundStyle(.purple, .blue)
                 .shadow(radius: 10, x:0, y:10)
             Image("ImagenVirtoria.png")
                 .resizable()
+                // .scaledToFill()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 400, height: 500)
+                // .clipped()
+                .clipShape(Circle())
+                .opacity(0.5)
+                .overlay(
+                    Text("Este es mi texto")
+                        .fontWeight(.black)
+                        .font(.system(.largeTitle, design: .rounded))
+                        .foregroundStyle(.black)
+                        .background(.purple)
+                        .opacity(1)
+                        .cornerRadius(25.0)
+                        .padding(50),
+                    alignment: .bottom
+                        
+                )
                 
         }
         
